fa8712a26910f3e798f7b4fea13d570704be72f2
Ziga <zigavidmar96@gmail.com>
2020-06-19 09:41:50 +0200

    starter files

---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    text: any;
+}
+
+export const Counter: React.FC<Props> = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
